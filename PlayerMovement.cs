d4eb4b60b21345242527c51ce48a22f5414198f7
Dylan Tuck <45218496+Keli2k@users.noreply.github.com>
2019-04-24 18:33:15 -0400

    Update PlayerMovement.cs

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -7,9 +7,6 @@ public class PlayerMovement : MonoBehaviour
   
     public Animator animator;
     public CharacterController2D controller;
-    private float dashTime;
-    public float dashSpeed;
-    public float startDashTime;
     private Rigidbody2D rb;
     private int direction;
     public float runSpeed = 40f;
@@ -19,7 +16,6 @@ public class PlayerMovement : MonoBehaviour
      void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        dashTime = startDashTime;
     }
     // Update is called once per frame
     void Update()
@@ -33,41 +29,6 @@ public class PlayerMovement : MonoBehaviour
             jump = true;
             animator.SetBool("IsJumping", true);
         }
-        if(direction == 0)
-        {
-            if(Input.GetKey(KeyCode.D))
-            {
-                direction = 1;
-            }
-            else if(Input.GetKey(KeyCode.A))
-            {
-                direction = 2;
-            }
-        }
-
-        else
-        {
-            if(dashTime <= 0 )
-            {
-                direction = 0;
-                rb.velocity = Vector2.zero;
-                dashTime = startDashTime;
-            }
-            if(direction == 1 && Input.GetMouseButton(1) && dashTime <= 0)
-            {
-                rb.velocity = Vector2.right * dashSpeed;
-                direction = 0;
-                
-            }
-            if (direction == 2 && Input.GetMouseButton(1) && dashTime <= 0)
-            { 
-                rb.velocity = Vector2.left * dashSpeed;
-                direction = 0;
-                
-            }
-            dashTime -= Time.deltaTime;
-        }
-        
 
     }
     public void OnLanding()
